c9653a84c2f42ae60dde2202ef52adc4ef6cd2ec
Yovador <yovador@gmail.com>
2020-10-01 14:58:30 +0200

    Fix Unity and git Please

---
+++ Assets/Scripts/LeverBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverBehaviour : InteractibleObject
+{
+
+    [SerializeField] private GameObject objectToControl;
+
+    public override void ActionOnInteraction()
+    {
+
+        if (Input.GetButtonDown("Use") && playerController.GetcharacterColor() == 0)
+        {
+            objectToControl.SetActive(!objectToControl.activeSelf);
+            Destroy(gameObject);
+        }
+    }
+}
