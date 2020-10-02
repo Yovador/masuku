dea58ae6eb1b76f905a66bf29c19569d446a48df
Marchosias6 <46958285+Marchosias6@users.noreply.github.com>
2020-10-02 17:45:30 +0200

    push menu

---
+++ Assets/PLAY.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class PLAY : MonoBehaviour
+{
+    public string LevelToLoad;
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(LevelToLoad);
+    }
+}
---
+++ Assets/QUIT.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class QUIT : MonoBehaviour
+{
+    public void quit()
+    {
+        Debug.Log("Has Quit Game");
+        Application.Quit();
+    }
+}
